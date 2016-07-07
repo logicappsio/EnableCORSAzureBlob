68d839b0007298a648f1e7906bf245a85e028632
Jeff Hollan <jeffhollan@outlook.com>
2016-07-07 13:17:25 -0700

    Add files via upload

---
+++ EnableCORSAzureBlob.ps1
@@ -0,0 +1,14 @@
+﻿clear 
+$StorageAccountName = "[storage_account_name]"
+$Key = "[storage_account_key]"
+$Context = New-AzureStorageContext -StorageAccountKey $Key -StorageAccountName $StorageAccountName
+$CorsRules = (@{
+    AllowedHeaders=@("*");
+    AllowedOrigins=@("*");
+    ExposedHeaders=@("content-length");
+    MaxAgeInSeconds=200;
+    AllowedMethods=@("Get","Connect", "Head")})
+Set-AzureStorageCORSRule -ServiceType Blob -CorsRules $CorsRules -Context $Context
+$CORSrule = Get-AzureStorageCORSRule -ServiceType Blob -Context $Context
+echo "Current CORS rules: "
+echo $CORSrule
